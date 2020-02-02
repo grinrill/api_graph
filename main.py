7b967c7f7afe133c3eb83af6d0fb59a874f67dc5
Кирилл Гринчук <kiralgrin@gmail.com>
2020-02-02 23:02:18 +0300

    add create_api_token method

--- main.py
+++ main.py
@@ -1,8 +1,28 @@
 from bottle import Bottle, request
 import json
+from html_telegraph_poster import TelegraphPoster
 
 app = Bottle()
-@app.get('/')
-def api():
-    my_dict = request.params.decode()
-    return json.dumpd(my_dict)
+
+graph = TelegraphPoster()
+
+@app.get('/create_api_token')
+def create_api_token():
+	kwargs = {
+		'short_name': None,
+		'author_name': None,
+		'author_url': None,
+	}
+
+	args_raw = request.params.decode().dict
+	args = {i: args_raw[i][0] for i in args_raw}
+
+	kwargs.update(args)
+	kwargs = {i: kwargs[i] for i in ['short_name', 'author_name', 'author_url']}
+
+	if kwargs['author_name'] == None:
+		return 400 #418 :)
+
+	return graph.create_api_token(**kwargs)
+
+app.run(host='0.0.0.0', port=8080)
