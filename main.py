d7f252e66770872f54283c9b850af2ce2496db77
Кирилл Гринчук <kiralgrin@gmail.com>
2020-02-06 23:47:36 +0300

    nate that /edit does not works :(

--- main.py
+++ main.py
@@ -34,8 +34,9 @@ def post():
 	if 'access_token' in args_raw:
 		access_token = args_raw['access_token'][0]
 	else:
-		print('returned')
-		return '400 need access_token.\nYou can get it with /create_api_token?short_name=<short_name>[&author_name=<author_name>&author_url=<author_url>]'
+		return '''400 need access_token.
+		You can get it with /create_api_token?short_name=<short_name>[&author_name=<author_name>&author_url=<author_url>]'''
+	graph = TelegraphPoster(access_token)
 
 	kwargs.update(args)
 	kwargs = {i: kwargs[i] for i in ['title', 'author', 'text']}
@@ -44,8 +45,56 @@ def post():
 		if kwargs[i] == None:
 			return f'field "{i}" cant be empty' #418 :)
 
-	graph = TelegraphPoster(access_token)
-	return graph.post(**kwargs)
+	print(graph)
+	post = graph.post(**kwargs)
+	post['state'] = vars(graph)
+	return post
 
+
+@app.get('/edit')
+def edit():
+	kwargs = {i: None for i in ['title', 'author', 'text']}
+
+	args_raw = request.params.decode().dict
+	args = {i: args_raw[i][0] for i in args_raw}
+
+	if 'access_token' in args_raw:
+		access_token = args_raw['access_token'][0]
+	else:
+		return '''400 need access_token.
+		You can get it with /create_api_token?short_name=<short_name>[&author_name=<author_name>&author_url=<author_url>]'''
+	graph = TelegraphPoster(access_token)
+
+	if 'state' in args_raw:
+		params = args_raw['state'][0]
+		for k, v in json.loads(params).items():
+			graph.__setattr__(k, v)
+	else: 
+		return '''400 need state.
+		You can get it with /post?title=<title>&author=<author>&text=<text>&access_token=<access_token>'''
+
+	# if 'tph_uuid' in args_raw:
+	# 	graph.tph_uuid = args_raw['tph_uuid'][0]
+	# else:
+	# 	return '''400 need tph_uuid.
+	# 	You can get it with /post?title=<title>&author=<author>&text=<text>&access_token=<access_token>'''
+	
+	# if 'path' in args_raw:
+	# 	graph.path = args_raw['path'][0]
+	# else:
+	# 	return '''400 need path.
+	# 	You can get it with /post?title=<title>&author=<author>&text=<text>&access_token=<access_token>'''
+
+	# if 'page_id' in args_raw:
+	# 	graph.page_id = args_raw['page_id'][0]
+	# else:
+	# 	return '''400 need page_id.
+	# 	You can get it with /post?title=<title>&author=<author>&text=<text>&access_token=<access_token>'''
+	
+	kwargs.update(args)
+	kwargs = {i: kwargs[i] for i in ['title', 'author', 'text']}
+
+	print(graph.path)
+	return graph.edit(**kwargs)
 
 app.run(host='0.0.0.0', port=8080)
